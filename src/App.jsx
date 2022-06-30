4551551855ac2c9287fd3b47e349d6734ecad5bc
zcarpen <zcarpen@gmail.com>
2022-06-30 06:31:10 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import FeedbackItem from "./components/FeedbackItem";
+import Header from './components/Header';
+
+function App() {
+    return (
+        <>
+            <Header />
+            <div className='container'>
+                <FeedbackItem />
+            </div>
+        </>
+    )
+}
+
+export default App;
